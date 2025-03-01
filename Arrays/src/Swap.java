e2dc01d971fbf12fc6a56f5efb6a0dfcd4fc969d
DanishHomavazir <danishhomavazir1@gmail.com>
2025-03-01 21:55:53 +0530

    Swapping in Array

---
+++ Arrays/src/Swap.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 8, 9, 12};
+        swap (arr,2,4);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr,int index1, int index2 ){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
