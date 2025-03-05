30380d173e84b52c0d7c9cd886edd783f21a3105
DanishHomavazir <danishhomavazir1@gmail.com>
2025-03-05 22:04:18 +0530

    Linear Search finding min

---
+++ Linear Search/src/FindMinInString.java
@@ -0,0 +1,18 @@
+public class FindMinInString {
+    public static void main(String[] args) {
+        int[] arr = {19,12,7,3,14};
+        System.out.println(min(arr));
+    }
+
+    //assuming that the array is not empty
+    //return the minimum value present in the array
+    static int min(int[] arr){
+        int ans = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<ans){
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+}
