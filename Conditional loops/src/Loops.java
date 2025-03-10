1c0e37aeb680a892202db557c2db1e6997d4c188
DanishHomavazir <danishhomavazir1@gmail.com>
2025-03-10 22:21:04 +0530

    Understanding Loops

---
+++ Conditional loops/src/Loops.java
@@ -0,0 +1,22 @@
+public class Loops {
+    public static void main(String[] args) {
+//        multiple if else statements
+//        int salary = 35000;
+//        if (salary > 20000){
+//            salary += 3000;
+//        } else if (salary > 15000) {
+//            salary += 2000;
+//        }
+//        else{
+//            salary += 1000;
+//        }
+//        System.out.println("The salary of the employee including bonus is:"+salary);
+//    }
+
+        int num = 1;
+        while (num <= 5) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
