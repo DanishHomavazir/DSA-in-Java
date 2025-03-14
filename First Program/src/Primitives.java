f9d1697788757de3d2dc37135359c815b5876299
DanishHomavazir <danishhomavazir1@gmail.com>
2025-03-14 19:38:20 +0530

    Primitive types

---
+++ First Program/src/Primitives.java
@@ -0,0 +1,16 @@
+public class Primitives {
+    public static void main(String[] args) {
+//        Primitive Data Types
+//        Primitive Data Types are those data types that cannot be broken down into further data types
+        int rollno = 24;
+        char letter = 'd';
+        float marks = 99.9f;
+        double largeDecimalNumbers = 6548436841245.32;
+         long largeInteger = 54842184142486454L;
+         boolean check = true;
+
+
+//         String is a non-primitive data type as it can be broken down into further data types
+//         like if the string is Danish then it can be further broken down into char of the letters in the name
+    }
+}
