d2002de853646a84a01aa9610f794a9b5be0617e
DanishHomavazir <danishhomavazir1@gmail.com>
2025-03-15 22:18:45 +0530

    Multiple methods to print the array

---
+++ Arrays/src/Multi_Arrays.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Multi_Arrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int arr[][] = new int[3][3];//creating a 2D array
+
+        //taking input in the array
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+
+        //1st method for printing the 2D array
+        for (int row = 0 ; row<arr.length; row++){
+            for (int col = 0 ; col< arr[row].length; col++){
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println(" ");
+        }
+
+        //2nd method for printing the 2D array
+        // enhanced for loop method (for-each loop method)
+        for (int[] a : arr ){
+            System.out.println(Arrays.toString(a));
+        }
+
+        //3rd method for printing the 2D array
+        for (int row = 0; row < arr.length; row++) {
+            System.out.println(Arrays.toString(arr[row]));
+        }
+    }
+}
