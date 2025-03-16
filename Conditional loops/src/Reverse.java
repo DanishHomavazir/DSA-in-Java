da8c1040f867d44e4beca32b168d4899129dcb85
DanishHomavazir <danishhomavazir1@gmail.com>
2025-03-16 22:16:15 +0530

    Reversing the loop

---
+++ Conditional loops/src/Reverse.java
@@ -0,0 +1,13 @@
+//Program to print the reverse of a given number
+public class Reverse {
+    public static void main(String[] args) {
+        int num = 476638;
+        int ans = 0;
+        while(num>0){
+            int rem = num%10;
+            num=num/10;
+            ans = ans * 10 + rem;
+        }
+        System.out.print("The reverse of the given number is: "+ans);
+    }
+}
