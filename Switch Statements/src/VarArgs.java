dc9d47a48eff5f111c67330461bc7462597400f9
DanishHomavazir <danishhomavazir1@gmail.com>
2025-03-20 22:10:20 +0530

    Variable Args

---
+++ Switch Statements/src/VarArgs.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun(2,5,9,5,88,77);
+        multiple(5,7,4,"Danish");
+    }
+    static void fun(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple(int a , int b , int c , String ...v){
+        System.out.println(Arrays.toString(v));
+    }
+}
